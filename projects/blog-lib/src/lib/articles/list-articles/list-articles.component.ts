89e042f64fe2afbc01760ac948d243e02159573a
Alexis Merceron <merceron.alexis.dev@gmail.com>
2018-06-01 22:45:37 +0200

    remove state and use interface for service

---
+++ projects/blog-lib/src/lib/articles/list-articles/list-articles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'amer-list-articles',
+  templateUrl: './list-articles.component.html',
+  styleUrls: ['./list-articles.component.css']
+})
+export class ListArticlesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {BlogLibModule} from "../../projects/blog-lib/src/lib/blog-lib.module";
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BlogLibModule,
+      ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   }));
