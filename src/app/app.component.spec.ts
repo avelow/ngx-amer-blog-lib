2ee0e7703307e8d15c9da12fb8bb3b85c44e7134
Alexis Merceron <merceron.alexis.dev@gmail.com>
2018-06-07 20:04:46 +0200

    ajout elevation sidebar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {BlogLibModule} from "../../projects/blog-lib/src/lib/blog-lib.module";
+import {BlogLibComponent} from "../../projects/blog-lib/src/lib/blog-lib.component";
+import {By} from "@angular/platform-browser";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -16,6 +18,8 @@ describe('AppComponent', () => {
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+    let sidebarElement = fixture.debugElement.query(By.css('div[sidebar]'));
     expect(app).toBeTruthy();
+    expect(sidebarElement).toBeTruthy();
   }));
 });
